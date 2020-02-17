26ec9972bd7fb1daf8650109647c1c418fa523e3
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-02-17 16:05:08 -0600

    Finalizado

--- ServiciosWeb.ClienteWeb/Controllers/SchoolController.cs
+++ ServiciosWeb.ClienteWeb/Controllers/SchoolController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using ServiciosWeb.Dominio;
+using System.Net.Http.Formatting;
 
 namespace ServiciosWeb.ClienteWeb.Controllers
 {
@@ -17,6 +18,7 @@ namespace ServiciosWeb.ClienteWeb.Controllers
             HttpClient clienteHTTP = new HttpClient
             {
                 BaseAddress = new Uri("http://localhost:60018/")
+                //BaseAddress = new Uri("http://localhost/WebApi/")
             };
 
             var request = clienteHTTP.GetAsync("api/School").Result;
@@ -29,5 +31,128 @@ namespace ServiciosWeb.ClienteWeb.Controllers
             }
             return View(new List<Dominio.Escuela>());
         }
+
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(Escuela escuela)
+        {
+            HttpClient clienteHTTP = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:60018/")
+                //BaseAddress = new Uri("http://localhost/WebApi/")
+            };
+
+            var request = clienteHTTP.PostAsync("api/School", escuela, new JsonMediaTypeFormatter()).Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(resultString);
+
+                if (correcto)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(escuela);
+            }
+
+            return View(escuela);
+        }
+
+        [HttpGet]
+        public ActionResult Actualizar(int id)
+        {
+            HttpClient clienteHTTP = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:60018/")
+                //BaseAddress = new Uri("http://localhost/WebApi/")
+            };
+
+            var request = clienteHTTP.GetAsync("api/School?id=" +  id).Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var informacion = JsonConvert.DeserializeObject<Escuela>(resultString);
+                return View(informacion);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Actualizar(Escuela escuela)
+        {
+            HttpClient clienteHTTP = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:60018/")
+                //BaseAddress = new Uri("http://localhost/WebApi/")
+            };
+
+            var request = clienteHTTP.PutAsync("api/School", escuela, new JsonMediaTypeFormatter()).Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(resultString);
+
+                if (correcto)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(escuela);
+            }
+
+            return View(escuela);
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            HttpClient clienteHTTP = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:60018/")
+                //BaseAddress = new Uri("http://localhost/WebApi/")
+            };
+
+            var request = clienteHTTP.DeleteAsync("api/School?id=" + id).Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(resultString);
+
+                if (correcto)
+                {
+                    return RedirectToAction("Index");
+                }          
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Detalle(int id)
+        {
+            HttpClient clienteHTTP = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:60018/")
+                //BaseAddress = new Uri("http://localhost/WebApi/")
+            };
+
+            var request = clienteHTTP.GetAsync("api/School?id=" + id).Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var informacion = JsonConvert.DeserializeObject<Escuela>(resultString);
+                return View(informacion);
+            }
+            return View();
+        }
+
     }
 }
